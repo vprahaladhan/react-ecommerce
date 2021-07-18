7a769c6d5b8df66c9ed62621fe9652b6c0282a08
vprahaladhan <vprahaladhan@users.noreply.github.com>
2021-07-18 10:23:41 +1000

    commit change 2 static.json file, rm svcWorker.js

--- src/index.js
+++ src/index.js
@@ -8,18 +8,20 @@ import 'react-toastify/dist/ReactToastify.css';
 import './index.css';
 
 import App from './App';
-import { register }  from './serviceWorker';
-import reportWebVitals from './reportWebVitals';
+// import { register }  from './serviceWorker';
+// import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
     <Router> 
       <App />
     </Router>    
-  </React.StrictMode>, document.getElementById('root'));
+  </React.StrictMode>, 
+  document.getElementById('root')
+);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
-register()
+// reportWebVitals();
+// register()
